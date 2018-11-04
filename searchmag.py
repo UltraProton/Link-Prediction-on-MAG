62bd47981f59989b950749082210ccdee0bb85c4
sunilsarode <sarodesunil7@gmail.com>
2018-11-05 00:17:42 +0530

    Add files via upload
    dblpjournals.txt  require txt file name change

--- searchmag.py
+++ searchmag.py
@@ -13,18 +13,18 @@ for line in open('dblpjournals.txt', 'r'):
     line = line.lower()
     venues[line] = line
 
-all_fields = ['authors']
-with open("Authors2.csv", "w") as output_file:
+all_fields = ['authors','year']
+with open("AuthorsYear.csv", "w") as output_file:
     writer = csv.DictWriter(output_file, all_fields)
     writer.writeheader()
 
     for index, row in df.iterrows():
         venue = row['venue']
-        if pandas.isnull(venue) or pandas.isnull(row['authors']):
+        if pandas.isnull(venue):
             continue
 
         venue = venue.lower()
 
         if venue in venues:
 
-            writer.writerow({'authors':row['authors']})
+            writer.writerow({'authors':row['authors'],'year':row['year']})
