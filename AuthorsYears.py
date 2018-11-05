0fd6f1c33b15fc76a8e9df52a2ed37b631cb7dbe
UltraProton <adityarksht64@gmail.com>
2018-11-05 18:27:08 +0530

    Modified AuthorsYears.py

--- AuthorsYears.py
+++ AuthorsYears.py
@@ -2,36 +2,75 @@ import pandas as pd
 from collections import defaultdict
 import ast
 import csv
-import  itertools
+import itertools
 
 df = pd.read_csv('AuthorsYear.csv')
 
 year_author_dict = defaultdict(list)
 author_year_dict = defaultdict(list)
 
-#first dictionary of authors and year
+# first dictionary of authors and year
 
 for index, row in df.iterrows():
     authors = ast.literal_eval(row["authors"])
-    key=authors[0]['name']
+    key = authors[0]['name']
     authors.pop(0)
-    year=row['year']
+    year = row['year']
     author_year_dict[key].append(year)
 
-print(author_year_dict)
+#print(author_year_dict)
 
-
-#second dictionary of year to authors
+# second dictionary of year to authors
 
 
 for index, row in df.iterrows():
     authors = ast.literal_eval(row["authors"])
-    author=authors[0]['name']
+    author = authors[0]['name']
     authors.pop(0)
-    key=row['year']
+    key = row['year']
     year_author_dict[key].append(author)
 
-print(year_author_dict)
+#print(year_author_dict)
 
 lengths = [len(v) for v in author_year_dict.values()]
-print(min(lengths))
+# print(max(lengths))
+
+m = 0
+a = ''
+for k, v in author_year_dict.items():
+    if m < len(v):
+        m = len(v)
+        a = k
+
+# print(a)
+
+
+unipartite = defaultdict(list)
+
+# unipartite graph for all year
+
+for author, yearlist in author_year_dict.items():
+    for year in yearlist:
+        list1 = year_author_dict[int(year)]
+        unipartite[author].extend(list1)
+
+# unipartite['Huanting Chen'].pop(0)
+
+A = unipartite['Huanting Chen'];
+#print(A)
+
+for year, authorlist in year_author_dict.items():
+    if int(year) not in author_year_dict['Huanting Chen']:
+        innerList = set(authorlist).intersection(A)
+        if len(innerList)>0:
+            print(year, innerList)
+
+# [1996, 2013, 1994]
+
+'''
+pattern=defaultdict(list)
+print(pattern)
+'''
+
+'''for key,val in unipartite.items():
+    print(key,val)'''
